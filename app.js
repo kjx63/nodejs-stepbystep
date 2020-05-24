ac54674e98313e2488ad228c04bb056278a6f461
kjx63 <ueyama.kenji.63@gmail.com>
2020-05-24 21:15:38 +0900

    Add users and index routes

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const logger = require('morgan');
 const index = require('./routes/index');
 const posts = require('./routes/posts');
 const reviews = require('./routes/reviews');
-const users = require('./routes/users');
 
 const app = express();
 
@@ -29,7 +28,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/posts', posts);
 app.use('/posts/:id/reviews', reviews);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
