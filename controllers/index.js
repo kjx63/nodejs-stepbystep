ac8859d3f1ca03cda31f949e5891bd587ecb8689
kjx63 <ueyama.kenji.63@gmail.com>
2020-05-24 22:06:03 +0900

    Add postRegister controller for Register User git add .Before mongoose connections

--- controllers/index.js
+++ controllers/index.js
@@ -3,6 +3,15 @@ const User = require('../models/user');
 module.exports = {
     // In ES6, this is how you create the method on this object{}
     postRegister(req, res, next) {
-        res.send('POST /register');
+        // https://github.com/saintedlama/passport-local-mongoose/blob/master/examples/login/routes.js
+        User.register(new User({ username: req.body.username }), req.body.password, (err) => {
+            if (err) {
+                console.log('error while user register!', err);
+                return next(err);
+            }
+            console.log('user registered!');
+
+            res.redirect('/');
+        });
     }
 }
