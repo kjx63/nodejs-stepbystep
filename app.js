616c963fd0d34abfcdf5dcf54ed22ae63ca2761e
kjx63 <ueyama.kenji.63@gmail.com>
2020-05-24 18:10:24 +0900

    Add posts routes

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const index = require('./routes/index');
+const posts = require('./routes/posts');
 const users = require('./routes/users');
 
 const app = express();
@@ -25,6 +26,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
+app.use('/posts', posts);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
