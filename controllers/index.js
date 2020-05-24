b0283761d776a61183a97a054a0dc89c0bd53d40
kjx63 <ueyama.kenji.63@gmail.com>
2020-05-25 00:16:12 +0900

    Refactor /login and /logout Routes to Index Controllers

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,8 @@
 const User = require('../models/user');
+const passport = require('passport')
 
 module.exports = {
+    // Post /register
     // In ES6, this is how you create the method on this object{}
     async postRegister(req, res, next) {
         // https://github.com/saintedlama/passport-local-mongoose/blob/master/examples/login/routes.js
@@ -12,6 +14,20 @@ module.exports = {
         });
 
         await User.register(newUser, req.body.password);
+        res.redirect('/');
+    },
+
+    // POST /login
+    postLogin(req, res, next) {
+        passport.authenticate('local', {
+            successRedirect: '/',
+            failureRedirect: '/login'
+        })(req, res, next);
+    },
+
+    // GET /logout
+    getLogout(req, res, next) {
+        req.logout();
         res.redirect('/');
     }
 }
