390d26f9cdc5073016f6fbc8c1112c980b31204a
kjx63 <ueyama.kenji.63@gmail.com>
2020-05-24 21:37:00 +0900

    Add review model

---
+++ models/review.js
@@ -0,0 +1,15 @@
+// Define Schema
+// - https://mongoosejs.com/docs/guide.html
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ReviewSchema = new Schema({
+    body: String,
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }
+});
+
+module.exports = mongoose.model('Review', ReviewSchema);
