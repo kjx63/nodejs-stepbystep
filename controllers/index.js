71b2fc9d9dc7b5d320c10564bd9f66820a5a3ac2
kjx63 <ueyama.kenji.63@gmail.com>
2020-05-24 22:29:13 +0900

    Add Email & Profile image to postRegister method

--- controllers/index.js
+++ controllers/index.js
@@ -4,7 +4,13 @@ module.exports = {
     // In ES6, this is how you create the method on this object{}
     postRegister(req, res, next) {
         // https://github.com/saintedlama/passport-local-mongoose/blob/master/examples/login/routes.js
-        User.register(new User({ username: req.body.username }), req.body.password, (err) => {
+        const newUser = new User({
+            username: req.body.username,
+            // Add other keys here
+            email: req.body.email,
+            image: req.body.image
+        });
+        User.register(newUser, req.body.password, (err) => {
             if (err) {
                 console.log('error while user register!', err);
                 return next(err);
