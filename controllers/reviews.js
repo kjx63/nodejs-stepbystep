d7fb23e8bc3504d8ec073d3012ece9a9cf6c9ba2
kjx63 <ueyama.kenji.63@gmail.com>
2020-05-27 14:00:57 +0900

    Setup revews controllers, review model and review routes

---
+++ controllers/reviews.js
@@ -0,0 +1,19 @@
+const Post = require('../models/post');
+const Review = require('../models/review');
+
+
+module.exports = {
+    // Review Create
+    async reviewCreate(req, res, next) {
+
+    },
+    // Review Update
+    async reviewUpdate(req, res, next) {
+
+    },
+
+    //Review Destroy 
+    async reviewDestroy(req, res, next) {
+
+    }
+}
